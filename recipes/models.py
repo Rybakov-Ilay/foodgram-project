e84a63c9251762505715d10443b8cf8ca85c21e0
Rybakov-Ilay <ilay106@yandex.ru>
2021-04-15 16:40:51 +0300

    Проверка на flake8 через workflows

--- recipes/models.py
+++ recipes/models.py
@@ -18,6 +18,8 @@ class Tag(TimeStampedModel):
 
     def __str__(self):
         return self.name
+
+
 class Ingredient(TimeStampedModel):
     name = models.CharField(
         verbose_name="название",
