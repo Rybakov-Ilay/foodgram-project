36266167e388152cd8f875c8c6b0a1b3b6672a47
Rybakov-Ilay <ilay106@yandex.ru>
2021-04-18 10:32:21 +0300

    Добавление фильтрации ( апгрейд )

--- users/admin.py
+++ users/admin.py
@@ -3,7 +3,7 @@ from django.contrib import admin
 from django.contrib.flatpages.admin import FlatPageAdmin
 from django.contrib.flatpages.models import FlatPage
 from django.db import models
-from django.contrib.auth.models import User
+from django.contrib.auth.admin import UserAdmin
 
 
 # Define a new FlatPageAdmin
@@ -15,15 +15,4 @@ class FlatPageAdmin(FlatPageAdmin):
 admin.site.unregister(FlatPage)
 admin.site.register(FlatPage, FlatPageAdmin)
 
-
-@admin.register(User)
-class UserAdmin(admin.ModelAdmin):
-    list_display = (
-        "username",
-        "email",
-        "first_name",
-        "last_name",
-        "is_staff",
-    )
-
-    list_filter = ("username", "email",)
+UserAdmin.list_filter += ('username', 'email')
