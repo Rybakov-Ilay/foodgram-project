9eceef48b0619cd61c5533094a744853cd998f0a
Rybakov-Ilay <ilay106@yandex.ru>
2021-04-20 13:03:32 +0300

    Используем related_name.

--- users/templatetags/user_filters.py
+++ users/templatetags/user_filters.py
@@ -1,11 +1,5 @@
 from django import template
 
-from recipes.models import Favorite, Purchase, Subscribe
-
-try:
-    from django.urls import NoReverseMatch, reverse
-except ImportError:
-    from django.core.urlresolvers import NoReverseMatch, reverse
 register = template.Library()
 
 
@@ -16,40 +10,22 @@ def addclass(field, css):
 
 @register.filter
 def is_favorite(value, author):
-    return Favorite.objects.filter(recipe=value, author=author).exists()
+    return author.favorites.filter(recipe=value, author=author).exists()
+
+
+@register.filter
+def is_purchase(value, author):
+    return author.purchases.filter(recipe=value, author=author).exists()
+
+
+@register.filter
+def is_subscribe(value, author):
+    return author.follower.filter(author=value).exists()
 
 
 @register.filter
 def check_tag(recipe):
     return list(recipe.tags.values_list('name', flat=True))
-
-
-@register.filter
-def is_purchase(value, author):
-    return Purchase.objects.filter(recipe=value, author=author).exists()
-
-
-@register.filter
-def is_subscribe(value, follower):
-    return Subscribe.objects.filter(author=value, follower=follower).exists()
-
-
-@register.filter
-def remove_from_list(obj, value):
-
-    return obj.remove(value)
-
-
-@register.simple_tag(takes_context=True)
-def active(context, pattern_or_urlname):
-    try:
-        pattern = reverse(pattern_or_urlname)
-    except NoReverseMatch:
-        pattern = pattern_or_urlname
-    path = context["request"].path
-    if pattern == path:
-        return "nav__item_active"
-    return ""
 
 
 @register.filter
